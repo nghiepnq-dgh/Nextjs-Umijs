add9cee9c45b7c352b57e005377e5774ff7b6c16
nghiepnq-dgh <nghiepnq@digitechglobalco.com>
2020-09-01 09:45:39 +0700

    [Hot-Fix] Add Server to start

--- Web-Customer/server.js
+++ Web-Customer/server.js
@@ -5,15 +5,16 @@ const next = require("next");
 const { parse } = require("url");
 
 const nextI18NextMiddleware = require("next-i18next/middleware").default;
-
+var path = require("path");
+const fs = require("fs");
 const nextI18next = require("./i18n");
 const cookieParser = require("cookie-parser");
 const port = parseInt(process.env.APP_PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
+const { promisify } = require("util");
 const handle = app.getRequestHandler();
-
-(async () => {
+const readFileAsync = pm2(fs.readFile)(async () => {
   await app.prepare();
   const server = express();
 
@@ -25,6 +26,11 @@ const handle = app.getRequestHandler();
   server.get("*", (req, res) => {
     const parsedUrl = parse(req.url, true);
     return handle(req, res, parsedUrl);
+  });
+
+  server.get("/dashboard", async (req, res) => {
+    const file = await readFileAsync("public/admin/index.html");
+    res.end(file);
   });
 
   server.listen(port, () => {
